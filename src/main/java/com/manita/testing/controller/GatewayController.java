21aaa741199bbf48a4b0d18bfa2b20cbf4a56c80
Elvi98 <70364657+Elvi98@users.noreply.github.com>
2021-02-18 21:14:37 -0500

    Update GatewayController.java

--- src/main/java/com/manita/testing/controller/GatewayController.java
+++ src/main/java/com/manita/testing/controller/GatewayController.java
@@ -3,6 +3,7 @@ package com.manita.testing.controller;
 import com.manita.testing.service.GatewayService;
 import com.mercadopago.exceptions.MPException;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GatewayController {
     private final GatewayService gatewayService;
 
+    @CrossOrigin("**")
     @PostMapping("gateways")
     public String generarPreferencia() throws MPException {
         return gatewayService.generarPreferencia();
